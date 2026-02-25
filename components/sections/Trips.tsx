30522fd005b5e4af7dcc08eb331e38cfd351037b
revanthrk2004 <revanthkkrishnan@gmail.com>
2026-02-25 14:46:51 +0000

    add

--- components/sections/Trips.tsx
+++ components/sections/Trips.tsx
@@ -201,7 +201,7 @@ export default function Trips({ cms }: { cms?: HomeData }) {
                         {t.title}
                       </div>
 
-  <div className="mt-2 pb-[2px] text-[12px] leading-[1.35] text-white/75 sm:text-xs line-clamp-2">
+  <div className="mt-2 pb-[6px] text-[12px] leading-[1.35] text-white/75 sm:text-xs line-clamp-2">
   {t.subtitle}
 </div>
 
